1d30f20fc1713a748e196fc55788cc24da31dcea
Joao Santana <joaosantana.ti@gmail.com>
2017-04-25 09:30:24 -0300

    Adding a box to main activity

---
+++ app/src/main/java/jonss/com/github/sharedpreferencessample/SettingsFragment.java
@@ -0,0 +1,8 @@
+package jonss.com.github.sharedpreferencessample;
+
+/**
+ * Created by joao on 25/04/17.
+ */
+
+public class SettingsFragment {
+}
---
+++ app/src/main/java/jonss/com/github/sharedpreferencessample/utils/ViewUtils.java
@@ -0,0 +1,7 @@
+package jonss.com.github.sharedpreferencessample.utils;
+
+/**
+ * Created by joao on 25/04/17.
+ */
+public class ViewUtils {
+}
